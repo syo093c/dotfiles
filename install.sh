71f447b6f7e80b03c6caec426821a019514cec4b
syo <syoc3lv@outlook.com>
2022-05-08 15:03:15 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eu
+
+#install plugins
+echo "install zsh plugins"
+mv zsh-autosuggestions ohmyzsh/plugins/
+mv zsh-syntax-highlighting ohmyzsh/plugins/
+mv ohmyzsh .oh-my-zsh
+
+#install to HOME
+echo "install to HOME"
+cp -r .oh-my-zsh .curlrc .p10k.zsh  .tmux.conf  .vimrc  .wgetrc  .zshrc \
+    ~
+mkdir -p ~/.config
+cp -r config/nvim
+ln -s ~/.vimrc ~/.nvimrc
