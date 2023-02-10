311aac34c096d7a9bfe0c69e181994fc3b00b75c
syo <syo093c@gmail.com>
2023-02-10 22:08:27 +0900

    update

--- config/xmonad/bin/autostart.sh
+++ config/xmonad/bin/autostart.sh
@@ -1,39 +1,14 @@
 #!/bin/bash
 
-#--- picom ---
-killall -9 picom
-picom --config $HOME/.config/xmonad/picom.conf &
-
-#--- polybar ---
-killall -9 polybar
-#polybar &
-
-#--- lofi ---
-killall -9 rofi
-#rofi &
 #--- utils ---
 # cursor
 xsetroot -cursor_name left_ptr
-
-# nigtron wallpaper
-killall -9 nitrogen
-nitrogen --restore &
-
-# ibus
-ibus-daemon --xim --replace
 
 # caps lock to esc for vim
 xmodmap -e 'clear Lock' -e 'keycode x042=Escape'
 
 #set keyboard repeat rate
 xset r rate 250 90
-
-#mute
-pactl set-sink-volume "alsa_output.pci-0000_02_00.3.Speakers" 0% 
-
-#macbook
-# brightness 
-brightnessctl -d acpi_video0 set 50
 #--------------------
 # trackpad nature scroll direction
 xinput set-prop 'bcm5974'  'libinput Natural Scrolling Enabled' 1 
@@ -43,3 +18,30 @@ xinput set-prop 'bcm5974'  "libinput Scrolling Pixel Distance" 20
 xinput set-prop 'bcm5974'  295 0.6
 # tap to click
 xinput set-prop 'bcm5974' 'libinput Tapping Enabled' 1 
+
+#--- picom ---
+#killall -9 picom
+picom --config $HOME/.config/xmonad/picom.conf &
+
+#--- polybar ---
+#killall -9 polybar
+#polybar &
+
+#--- lofi ---
+#killall -9 rofi
+#rofi &
+
+#mute
+pactl set-sink-volume "alsa_output.pci-0000_02_00.3.Speakers" 0% 
+
+#macbook
+# brightness 
+brightnessctl -d acpi_video0 set 50
+
+# nigtron wallpaper
+killall -9 nitrogen
+nitrogen --restore &
+
+
+# ibus @IMPORTANT@ please put this at last, or xinput will conflict.
+ibus-daemon --xim --replace
