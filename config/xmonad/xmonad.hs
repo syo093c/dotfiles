965c636e894a776a545b23298963b43b722e5f98
syo <syo093c@gmail.com>
2023-02-12 01:04:32 +0900

    update

--- config/xmonad/xmonad.hs
+++ config/xmonad/xmonad.hs
@@ -224,19 +224,23 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList
 --
 --myLayout = tiled ||| Mirror tiled ||| noBorders Full
 --myLayout = mkToggle1 NBFULL $ mkToggle1 NOBORDERS $ tiled ||| Mirror tiled ||| noBorders Full
-myLayout = mkToggle1 NOBORDERS $ mkToggle1 NBFULL $ tiled ||| Mirror tiled ||| Full
-  where
-     -- default tiling algorithm partitions the screen into two panes
-     tiled   = Tall nmaster delta ratio
+myLayout = 
+    avoidStruts $
+    mkToggle1 NOBORDERS $ 
+    mkToggle1 NBFULL $ 
+    tiled ||| Mirror tiled ||| Full
+        where
+            -- default tiling algorithm partitions the screen into two panes
+            tiled   = Tall nmaster delta ratio
 
-     -- The default number of windows in the master pane
-     nmaster = 1
+            -- The default number of windows in the master pane
+            nmaster = 1
 
-     -- Default proportion of screen occupied by master pane
-     ratio   = 1/2
+            -- Default proportion of screen occupied by master pane
+            ratio   = 1/2
 
-     -- Percent of screen to increment by when resizing panes
-     delta   = 3/100
+            -- Percent of screen to increment by when resizing panes
+            delta   = 3/100
 
 ------------------------------------------------------------------------
 -- Window rules:
@@ -310,7 +314,7 @@ main = do
         keys               = myKeys,
         mouseBindings      = myMouseBindings,
       -- hooks, layouts
-        layoutHook         = avoidStruts $ myLayout,
+        layoutHook         =   myLayout,
         --layoutHook         = myLayout,
         manageHook         = myManageHook,
         handleEventHook    = myEventHook,
