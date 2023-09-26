9e9b2e312cfda1a8597bde1465c478747bc86a76
syo <syo@mercury.local>
2023-09-26 11:56:05 +0900

    update macos

---
+++ macos.sh
@@ -0,0 +1,15 @@
+defaults write com.apple.finder CreateDesktop false
+defaults write com.apple.Terminal FocusFollowsMouse -bool true
+defaults write  -g FocusFollowsMouse -bool true
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool TRUE
+defaults write com.apple.desktopservices DSDontWriteStores -bool TRUE
+defaults write -g ApplePressAndHoldEnabled -bool flase
+defaults write com.apple.finder AppleShowAllFiles TRUE
+defaults write com.apple.dock autohide-delay -float 2147483640
+defaults write com.apple.dock no-bouncing -bool TRUE
+
+# rectangle
+# autoraise
+# flameshot
+# copyq
