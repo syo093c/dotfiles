60042bdf9660583fe3e7dc72120512d92ebaba79
syo <syo@mercury.local>
2023-10-01 20:55:48 +0900

    update

--- macos.sh
+++ macos.sh
@@ -1,3 +1,5 @@
+#! /bin/bash -eu
+
 defaults write com.apple.finder CreateDesktop false
 defaults write com.apple.Terminal FocusFollowsMouse -bool true
 defaults write  -g FocusFollowsMouse -bool true
@@ -8,6 +10,13 @@ defaults write -g ApplePressAndHoldEnabled -bool flase
 defaults write com.apple.finder AppleShowAllFiles TRUE
 defaults write com.apple.dock autohide-delay -float 2147483640
 defaults write com.apple.dock no-bouncing -bool TRUE
+#---
+sudo pmset -a powernap 0
+sudo pmset -a tcpkeepalive 0
+sudo pmset -a ttyskeepawake 0
+sudo pmset -a womp 0
+sudo pmset repeat cancel
+sudo pmset schedule cancelall
 
 # rectangle
 # autoraise
