751e722b5f99907186108afdd5af03715d89cb49
syo093c <45763662+syo093c@users.noreply.github.com>
2023-12-28 12:59:00 +0900

    Update macos.sh

--- macos.sh
+++ macos.sh
@@ -6,7 +6,7 @@ defaults write  -g FocusFollowsMouse -bool true
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool TRUE
 defaults write com.apple.desktopservices DSDontWriteStores -bool TRUE
-defaults write -g ApplePressAndHoldEnabled -bool flase
+defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write com.apple.finder AppleShowAllFiles TRUE
 defaults write com.apple.dock autohide-delay -float 2147483640
 defaults write com.apple.dock no-bouncing -bool TRUE
