660eeeb11fbf8b0cfc12d34194d3cb8cb8fd1912
syo <syo093c@gmail.com>
2024-02-23 11:41:38 +0900

    update

--- macos.sh
+++ macos.sh
@@ -10,6 +10,11 @@ defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write com.apple.finder AppleShowAllFiles TRUE
 defaults write com.apple.dock autohide-delay -float 2147483640
 defaults write com.apple.dock no-bouncing -bool TRUE
+
+#sonoma input ui
+defaults write kCFPreferencesAnyApplication TSMLanguageIndicatorEnabled 0
+
+
 #-- KeyRepeat
 defaults write -g KeyRepeat -int 1
 defaults write -g InitialKeyRepeat -int 15
